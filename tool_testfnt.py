880fe41bfd724bf543ba0662e7127cc40572f9da
ws-3917 <1291934577@qq.com>
2024-04-09 13:09:15 +0800

    .

--- tool_testfnt.py
+++ tool_testfnt.py
@@ -5,18 +5,19 @@ import numpy as np
 
 # 字体路径、待显示字符、字体大小、绘图类型
 # '1'代表二值图（黑白），'L'代表灰度图
-fontfile = "fnt_zh-cn/fzsejt.ttf"   
-ch = "魖"
-size = 18
-glyphtype = '1'
+fontfile = "fnt_en/determination-mono.woff"   
+ch = "æ"
+size = 16
+glyphtype = 'L'
+threshold = 160     # 二值化阈值
 
 font = ImageFont.truetype(fontfile, size)
 
 # startpoint 为左上角绘制起点，endpoint为右下角绘制终点（图块大小）
 # 可以用getbbox计算，也可以直接手动指定
 
-#startpoint = (0, 0)
-startpoint = font.getbbox(ch)[:2]
+startpoint = (0, -1)
+#startpoint = font.getbbox(ch)[:2]
 #endpoint = (18, 18)
 endpoint = font.getbbox(ch)[2:]
 
@@ -29,9 +30,11 @@ drawtool.text(startpoint, ch, font=font, fill=255)
 arr = np.array(newimg)
 # 创建一个新数组用于存储灰度和透明度
 new_arr = np.empty((arr.shape[0], arr.shape[1], 2), dtype=np.uint8)
-new_arr[..., 0] = new_arr[..., 1] = (arr>200)*255   # 设置灰度和透明度
+new_arr[..., 0] = (arr>threshold)*255   # 设置灰度和透明度
 
-# 预览时不方便查看透明图，可以将下面这一行代码注释掉，注释后透明化处理将不写入图片，预览为黑色背景
+new_arr[..., 1] = 255   # 黑色背景
+#new_arr[..., 1] = new_arr[0]   # 透明背景
+
 newimg = Image.fromarray(new_arr, "LA")
 #newimg.save("test.png")
 newimg.show()
