fcff56abee66e4e687123888c4b20add62a7c095
WS-3917 <ws3917@outlook.com>
2024-06-02 22:24:03 +0800

    字频表。

---
+++ tool_writematch.py
@@ -0,0 +1,13 @@
+import re
+
+# 读取文件内容
+with open('charfreq.txt', 'r', encoding='utf-8') as file:
+    content = file.read()
+
+# 匹配所有汉字字符
+pattern = re.compile(r'\t[\u4e00-\u9fa5]')
+matches = pattern.findall(content)
+
+# 将匹配结果写入新文件
+with open('charfreq.txt', 'w', encoding='utf-8') as output_file:
+    output_file.write(''.join(matches))
