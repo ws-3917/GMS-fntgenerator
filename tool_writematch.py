ae0ffe7bc4ca5a4ba2683423f44df6c81d4d4aa7
WS-3917 <ws3917@outlook.com>
2024-06-02 22:26:45 +0800

    .

--- tool_writematch.py
+++ tool_writematch.py
@@ -1,13 +1,13 @@
 import re
 
 # 读取文件内容
-with open('charfreq.txt', 'r', encoding='utf-8') as file:
+with open('charfreq_tw.txt', 'r', encoding='utf-8') as file:
     content = file.read()
 
 # 匹配所有汉字字符
-pattern = re.compile(r'\t[\u4e00-\u9fa5]')
+pattern = re.compile(r' [\u4e00-\u9fa5]')
 matches = pattern.findall(content)
 
 # 将匹配结果写入新文件
-with open('charfreq.txt', 'w', encoding='utf-8') as output_file:
+with open('charfreq_tw.txt', 'w', encoding='utf-8') as output_file:
     output_file.write(''.join(matches))
